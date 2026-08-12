9889e1f3250b546c771a500ffbbedf84c82285e7
delightfactory <assalama962@gmail.com>
2026-08-13 00:20:18 +0300

    Correct generated Supabase type helper

--- lib/supabase/database.types.ts
+++ lib/supabase/database.types.ts
@@ -733,7 +733,7 @@ export type CompositeTypes<
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
